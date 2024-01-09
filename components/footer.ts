6bc59749bbcea2ab136423c9cd8cd87828e7c5cb
zTouu <ninvitou@gmail.com>
2024-01-09 11:40:09 +0700

    update ts files

---
+++ components/footer.ts
@@ -0,0 +1,71 @@
+export function footer(){
+    return `
+    <footer>
+    <div class="container">
+      <div class="wrapper">
+        <a href="index.html" class="footer-logo">
+          <img
+            src="./assets/images/logo-light.svg"
+            alt="DevBlog's Logo"
+            width="150"
+            height="40"
+            class="logo-light"
+          />
+          <img
+            src="./assets/images/logo-dark.svg"
+            alt="DevBlog's Logo"
+            width="150"
+            height="40"
+            class="logo-dark"
+          />
+        </a>
+  
+        <p class="footer-text">
+          Learn about Web accessibility, Web performance, and Database
+          management.
+        </p>
+      </div>
+  
+      <div class="wrapper">
+        <p class="footer-title">Quick Links</p>
+  
+        <ul>
+          <li>
+            <a href="#contact" class="footer-link">Advertise with us</a>
+          </li>
+  
+          <li>
+            <a href="#" class="footer-link">About Us</a>
+          </li>
+  
+          <li>
+            <a href="#contact" class="footer-link">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+  
+      <div class="wrapper">
+        <p class="footer-title">Legal Stuff</p>
+  
+        <ul>
+          <li>
+            <a href="#" class="footer-link">Privacy Notice</a>
+          </li>
+  
+          <li>
+            <a href="#" class="footer-link">Cookie Policy</a>
+          </li>
+  
+          <li>
+            <a href="#" class="footer-link">Terms Of Use</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  
+    <p class="copyright">
+      &copy; Copyright 2023 <a href="index.html">ninvitou</a>
+    </p>
+  </footer>
+    `
+  }
---
+++ components/header.ts
@@ -0,0 +1,100 @@
+export function header(){
+    return`
+    <header>
+    <div class="container">
+      <nav class="navbar">
+        <a href="index.html">
+          <img
+            src="./assets/images/logo-light.svg"
+            alt="Devblog's logo"
+            width="150"
+            height="40"
+            class="logo-light"
+          />
+          <img
+            src="./assets/images/logo-dark.svg"
+            alt="Devblog's logo"
+            width="150"
+            height="40"
+            class="logo-dark"
+          />
+        </a>
+  
+        <div class="btn-group">
+          <button class="theme-btn theme-btn-mobile light">
+            <ion-icon name="moon" class="moon"></ion-icon>
+            <ion-icon name="sunny" class="sun"></ion-icon>
+          </button>
+  
+          <button class="nav-menu-btn">
+            <ion-icon name="menu-outline"></ion-icon>
+          </button>
+        </div>
+  
+        <div class="flex-wrapper">
+          <ul class="desktop-nav">
+            <li>
+              <a href="index.html" class="nav-link">Home</a>
+            </li>
+  
+            <li>
+              <a href="#" class="nav-link">About Me</a>
+            </li>
+  
+            <li>
+              <a href="#contact" class="nav-link">Contact</a>
+            </li>
+          </ul>
+  
+          <button class="theme-btn theme-btn-desktop light">
+            <ion-icon name="moon" class="moon"></ion-icon>
+            <ion-icon name="sunny" class="sun"></ion-icon>
+          </button>
+        </div>
+  
+        <div class="mobile-nav">
+          <button class="nav-close-btn">
+            <ion-icon name="close-outline"></ion-icon>
+          </button>
+  
+          <div class="wrapper">
+            <p class="h3 nav-title">Main Menu</p>
+  
+            <ul>
+              <li class="nav-item">
+                <a href="index.html" class="nav-link">Home</a>
+              </li>
+  
+              <li class="nav-item">
+                <a href="#" class="nav-link">About Me</a>
+              </li>
+  
+              <li class="nav-item">
+                <a href="#contact" class="nav-link">Contact</a>
+              </li>
+            </ul>
+          </div>
+  
+          <div>
+            <p class="h3 nav-title">Topics</p>
+  
+            <ul>
+              <li class="nav-item">
+                <a href="SecureDB.html" class="nav-link">Database</a>
+              </li>
+  
+              <li class="nav-item">
+                <a href="accessTips.html" class="nav-link">Accessibility</a>
+              </li>
+  
+              <li class="nav-item">
+                <a href="tool2024.html" class="nav-link">Web Performance</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>`
+  }
+ 
