0ba327e75a33efff1be324472132cd6d4e269661
zTouu <ninvitou@gmail.com>
2024-01-10 17:49:10 +0700

    import navPart

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 import { header } from "./components/header";
 import { footer } from "./components/footer";
+import { navPart } from "./components/navPart";
 //import { loadMore } from "./components/loadMore";
 //import { marked } from "marked"
 //loadMore()
+//navPart()
 
 //document.body.insertAdjacentHTML('beforeend',await marked.parse('# Marked in the browser\n\nRendered by marked.'));
 
