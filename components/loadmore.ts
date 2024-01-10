97e8d75b7b8587b224ccc825b4427c0826a59b83
zTouu <ninvitou@gmail.com>
2024-01-10 10:14:22 +0700

    adjust indents

--- components/loadmore.ts
+++ components/loadmore.ts
@@ -1,17 +1,16 @@
 export function loadMore(){
-      let loadMoreBtn = document.querySelector('.load-more') as HTMLElement;
-      let currentItem = 4;
-  
-      loadMoreBtn.onclick = () =>{
-        let boxes = [...document.querySelectorAll('.blog .blog-card-group .blog-card')] as HTMLElement[];
-        for (var i = currentItem; i < currentItem + 4; i++){
-          boxes[i].style.display = 'grid';
-        }
-        currentItem += 4;
-  
-        if(currentItem >= boxes.length){
-          loadMoreBtn.style.display = 'none';
-        }
+    let loadMoreBtn = document.querySelector('.load-more') as HTMLElement;
+    let currentItem = 4;
+
+    loadMoreBtn.onclick = () =>{
+      let boxes = [...document.querySelectorAll('.blog .blog-card-group .blog-card')] as HTMLElement[];
+      for (var i = currentItem; i < currentItem + 4; i++){
+        boxes[i].style.display = 'grid';
       }
-    
+      currentItem += 4;
+
+      if(currentItem >= boxes.length){
+        loadMoreBtn.style.display = 'none';
+      }
+    }
   }
