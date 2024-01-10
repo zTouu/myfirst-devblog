80f864eece5af4dc5d0b4bbaa339baeeaf0521c5
zTouu <ninvitou@gmail.com>
2024-01-10 18:06:50 +0700

    second adjustment

--- main.ts
+++ main.ts
@@ -7,7 +7,10 @@ import { navPart } from "./components/navPart";
 //navPart()
 
 //document.body.insertAdjacentHTML('beforeend',await marked.parse('# Marked in the browser\n\nRendered by marked.'));
+document.getElementById('header')?.insertAdjacentHTML('beforebegin', header());
+navPart()
+document.getElementById('footer')?.insertAdjacentHTML('beforebegin', footer());
 
-
-
-
+fetch('/contentHtmls/indexContent.html')
+  .then((res) => res.text())
+  .then((text) => document.getElementById('content')?.insertAdjacentHTML('beforebegin', text))
