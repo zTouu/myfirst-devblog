b217a17fee120f0d045036b5cab10eea24a6c879
zTouu <ninvitou@gmail.com>
2024-01-11 19:58:19 +0700

    Loadmore Button and CSS for blogs

--- main.ts
+++ main.ts
@@ -1,16 +1,31 @@
 import { header } from "./components/header";
 import { footer } from "./components/footer";
 import { navPart } from "./components/navPart";
-//import { loadMore } from "./components/loadMore";
+import { loadMore } from "./components/loadMore";
 //import { marked } from "marked"
-//loadMore()
-//navPart()
 
 //document.body.insertAdjacentHTML('beforeend',await marked.parse('# Marked in the browser\n\nRendered by marked.'));
+
 document.getElementById('header')?.insertAdjacentHTML('beforebegin', header());
 navPart()
 document.getElementById('footer')?.insertAdjacentHTML('beforebegin', footer());
 
-fetch('/contentHtmls/indexContent.html')
-  .then((res) => res.text())
-  .then((text) => document.getElementById('content')?.insertAdjacentHTML('beforebegin', text))
+//content
+function loadAndRenderPage(pathname: string) {
+  if (pathname == "/") {
+    pathname = "/index.html"
+  }
+  const url = pathname.replace(/\/([\d\D]+?).html/, "/contentHtmls/$1Content.html")
+  fetch(url)
+    .then((res) => res.text())
+    .then((text) => {
+      console.log(text)
+      document.getElementById('content')?.insertAdjacentHTML('beforebegin', text)
+      loadMore()
+    })
+}
+
+window.addEventListener('locationchange', function () {
+  loadAndRenderPage(location.pathname)
+});
+loadAndRenderPage(location.pathname)
