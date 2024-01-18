5776656e0c15526e97ab001b3decf6621f1dda80
zTouu <ninvitou@gmail.com>
2024-01-18 14:42:28 +0700

    create loadAndRenderPage.ts

---
+++ src/components/loadAndRenderPage.ts
@@ -0,0 +1,14 @@
+import { loadMore } from "./loadMore"
+
+export function loadAndRenderPage(pathname: string = location.pathname) {
+    if (pathname == "/") {
+    pathname = "/index.html"
+    }
+    const url = pathname.replace(/\/([\d\D]+?).html/, "/contentHtmls/$1Content.html")
+    fetch(url)
+    .then((res) => res.text())
+    .then((text) => {
+        document.getElementById('content')?.insertAdjacentHTML('beforebegin', text)
+        loadMore()
+    })
+}
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,11 @@
 import { header } from "./components/header";
 import { footer } from "./components/footer";
 import { navPart } from "./components/navPart";
-import { loadMore } from "./components/loadMore";
-//import { marked } from "marked"
-
-//document.body.insertAdjacentHTML('beforeend',await marked.parse('# Marked in the browser\n\nRendered by marked.'));
+import { loadAndRenderPage } from "./components/loadAndRenderPage";
 
 document.getElementById('header')?.insertAdjacentHTML('beforebegin', header());
 navPart()
 document.getElementById('footer')?.insertAdjacentHTML('beforebegin', footer());
-
-//content
-function loadAndRenderPage(pathname: string = location.pathname) {
-  if (pathname == "/") {
-    pathname = "/index.html"
-  }
-  const url = pathname.replace(/\/([\d\D]+?).html/, "/contentHtmls/$1Content.html")
-  fetch(url)
-    .then((res) => res.text())
-    .then((text) => {
-      //console.log(text)
-      document.getElementById('content')?.insertAdjacentHTML('beforebegin', text)
-      loadMore()
-    })
-}
 
 window.addEventListener('locationchange', function () {
   loadAndRenderPage()
