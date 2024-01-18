aa0f88a6e4f24c342cfe85e93eee1caf0e0b1ae8
zTouu <ninvitou@gmail.com>
2024-01-18 14:52:42 +0700

    adjust indents

--- src/components/loadAndRenderPage.ts
+++ src/components/loadAndRenderPage.ts
@@ -2,13 +2,13 @@ import { loadMore } from "./loadMore"
 
 export function loadAndRenderPage(pathname: string = location.pathname) {
     if (pathname == "/") {
-    pathname = "/index.html"
+      pathname = "/index.html"
     }
     const url = pathname.replace(/\/([\d\D]+?).html/, "/contentHtmls/$1Content.html")
     fetch(url)
-    .then((res) => res.text())
-    .then((text) => {
+      .then((res) => res.text())
+      .then((text) => {
         document.getElementById('content')?.insertAdjacentHTML('beforebegin', text)
         loadMore()
-    })
+      })
 }
